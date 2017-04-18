175ca3d8dac7b8a5ec43b8fdb95cdb9fe0ec1dae
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2017-04-18 15:30:48 +0700

    Create sample01.py

---
+++ sample01.py
@@ -0,0 +1,13 @@
+# sample01.py : create window
+
+from tkinter import *
+
+# create windows
+root = Tk()
+
+# modified root wondows
+root.title("Lebeller")
+root.geometry("400x200")
+
+# kick off the event loop
+root.mainloop()
